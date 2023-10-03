f108a9adc4c5b2e5eb50b558f1363665264a5a00
ishi <berugen1st@gmail.com>
2023-10-03 09:39:55 +0700

    add write_into for handling mkdir, mkfile and update file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{env, fs, fs::File, path::Path};
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+const HOME: &str = "HOME";
+const DEFAULT_PATH: &str = ".local/share/mem";
+
+fn main() {}
+
+pub fn write_into(file_name: &str) -> Result<(), std::io::Error> {
+    let home_dir = env::var(HOME).expect("should been read `HOME` value");
+    let path = format!("{home_dir}/{DEFAULT_PATH}");
+    let file_path = format!("{home_dir}/{DEFAULT_PATH}/{file_name}");
+    let is_dir = Path::new(&path).is_dir();
+    let is_file = Path::new(&file_path).is_file();
+    if !is_dir {
+        fs::create_dir(&path)?;
+    }
+    if !is_file {
+        File::create(path)?;
+    }
+    Ok(())
 }
