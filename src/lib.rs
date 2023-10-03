10dd2d17a1b41c5023acf00d8c613fa85fd1fed8
ishi <berugen1st@gmail.com>
2023-10-04 05:57:05 +0700

    add JSONHandler for read and write content

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 mod constants;
 use constants::{DEFAULT_PATH, HOME};
-use std::{env, fs, fs::File, path::Path};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::{
+    env, fs,
+    fs::{File, OpenOptions},
+    path::Path,
+};
 
-pub struct MemorizeBox;
+#[derive(Serialize, Deserialize)]
+pub struct MemorizeBox {
+    pub alias: String,
+    pub command: String,
+}
 
 pub trait MemorizeUtils {
     fn validate_default_path(self, file_name: &str) -> Result<(), std::io::Error>;
@@ -23,5 +33,32 @@ impl MemorizeUtils for MemorizeBox {
 
     fn update(self, content: &str) -> Result<String, String> {
         Ok(String::from("Modified"))
+    }
+}
+
+pub struct JSONHandler<'j> {
+    file_path: &'j str,
+}
+
+impl<'j> JSONHandler<'j> {
+    pub fn new(file_path: &'j str) -> Self {
+        JSONHandler { file_path }
+    }
+    pub fn read_json_from_file(self) -> Result<Value, std::io::Error> {
+        let file = File::open(self.file_path)?;
+        let reader = std::io::BufReader::new(file);
+        let content = serde_json::from_reader(reader)?;
+        Ok(content)
+    }
+
+    pub fn write_into_json(self, content: &Value) -> Result<(), std::io::Error> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(self.file_path)?;
+        let writer = std::io::BufWriter::new(file);
+        serde_json::to_writer_pretty(writer, content)?;
+        Ok(())
     }
 }
