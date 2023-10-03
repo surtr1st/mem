ab49617c06cf0945933f0ce3b6e0c330e125df23
ishi <berugen1st@gmail.com>
2023-10-04 05:06:27 +0700

    update logic and change `write_into` to `validate_default_path`

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,13 @@ mod constants;
 use constants::{DEFAULT_PATH, HOME};
 use std::{env, fs, fs::File, path::Path};
 
-pub fn write_into(file_name: &str) -> Result<(), std::io::Error> {
+pub fn validate_default_path(file_name: &str) -> Result<(), std::io::Error> {
     let home_dir = env::var(HOME).expect("should been read `HOME` value");
     let path = format!("{home_dir}/{DEFAULT_PATH}");
     let file_path = format!("{home_dir}/{DEFAULT_PATH}/{file_name}");
-    let is_dir = Path::new(&path).is_dir();
-    let is_file = Path::new(&file_path).is_file();
-    if !is_dir {
+    if !Path::new(&path).is_dir() {
         fs::create_dir(&path)?;
     }
-    if !is_file {
-        File::create(path)?;
-    }
+    File::create(file_path)?;
     Ok(())
 }
