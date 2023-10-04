b5adc4051f15014f863712c705553dbaff764307
ishi <berugen1st@gmail.com>
2023-10-04 15:16:59 +0700

    change command structure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod subcommands;
-use anyhow::Result;
+use anyhow::{Error, Result};
 use clap::{command, Parser};
 use mem::{MemorizeBox, MemorizeUtils};
-use subcommands::MemorizeSubcommands;
+use subcommands::{GlobalArgs, MemorizeSubcommands};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = "A CLI tool used for `memorize` command-lines", long_about = None)]
 struct Memorize {
     #[command(subcommand)]
     subcommands: Option<MemorizeSubcommands>,
+
+    #[command(flatten)]
+    options: GlobalArgs,
 }
 
 fn main() -> Result<()> {
@@ -16,34 +19,51 @@ fn main() -> Result<()> {
 
     MemorizeUtils::validate_default_path()?;
 
+    let alias = mem.options.alias;
+    let command = mem.options.command;
+
     match &mem.subcommands {
-        Some(MemorizeSubcommands::Add { alias, command }) => {
+        Some(MemorizeSubcommands::Add) => {
+            let Some(a) = alias else {
+                return Err(Error::msg("Alias is empty!"));
+            };
+            let Some(c) = command else {
+                return Err(Error::msg("Command is empty!"));
+            };
             let memo_box = MemorizeBox {
-                alias: alias.to_string(),
-                command: command.to_string(),
+                alias: a.to_string(),
+                command: c.to_string(),
             };
             MemorizeUtils::add(&memo_box)?;
             Ok(())
         }
-        Some(MemorizeSubcommands::Del { alias }) => {
-            MemorizeUtils::delete_command_by_alias(&alias)?;
+        Some(MemorizeSubcommands::Del) => {
+            let Some(a) = alias else {
+                return Err(Error::msg("Alias is empty!"));
+            };
+            MemorizeUtils::delete_command_by_alias(&a)?;
             Ok(())
         }
         Some(MemorizeSubcommands::Set {
-            alias,
             new_alias,
             new_command,
         }) => {
+            let Some(a) = alias else {
+                return Err(Error::msg("Alias is empty!"));
+            };
             if let Some(na) = new_alias {
-                MemorizeUtils::update_by_alias(&alias, na)?;
+                MemorizeUtils::update_by_alias(&a, na)?;
             }
             if let Some(nc) = new_command {
-                MemorizeUtils::update_command_by_alias(&alias, nc)?;
+                MemorizeUtils::update_command_by_alias(&a, nc)?;
             }
             Ok(())
         }
-        Some(MemorizeSubcommands::Use { alias, value }) => {
-            MemorizeUtils::invoke_command(&alias, value)?;
+        Some(MemorizeSubcommands::Use { value }) => {
+            let Some(a) = alias else {
+                return Err(Error::msg("Alias is empty!"));
+            };
+            MemorizeUtils::invoke_command(&a, value)?;
             Ok(())
         }
         Some(MemorizeSubcommands::List) => {
--- src/subcommands.rs
+++ src/subcommands.rs
@@ -1,29 +1,13 @@
-use clap::{arg, Subcommand};
+use clap::{arg, Args, Subcommand};
 
 #[derive(Subcommand, Debug)]
 pub enum MemorizeSubcommands {
     /// Adding and memorize command
-    Add {
-        /// Specific command to be memorized
-        #[arg(short, long)]
-        command: String,
-
-        /// Set alias for a command
-        #[arg(short, long)]
-        alias: String,
-    },
+    Add,
     /// Delete the specific memorized command
-    Del {
-        /// Specific command to be memorized
-        #[arg(short, long)]
-        alias: String,
-    },
+    Del,
     /// Update the specific memorized command
     Set {
-        /// Set alias for a command
-        #[arg(short, long)]
-        alias: String,
-
         /// Set new value for alias
         #[arg(short = 'n', long)]
         new_alias: Option<String>,
@@ -34,13 +18,21 @@ pub enum MemorizeSubcommands {
     },
     /// Execute the target memorized command by its alias
     Use {
-        /// Set alias for a command
-        alias: String,
-
         /// Set value for a command
         #[arg(short = 'v', long)]
         value: String,
     },
     /// Show a list of memorized commands and its alias
     List,
+}
+
+#[derive(Args, Debug)]
+pub struct GlobalArgs {
+    /// Specific command to be memorized
+    #[arg(short, long, global = true)]
+    pub command: Option<String>,
+
+    /// Set alias for a command
+    #[arg(short, long, global = true)]
+    pub alias: Option<String>,
 }
