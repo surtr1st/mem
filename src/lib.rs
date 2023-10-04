7f6669a0761d71b829c2bc8f3635b4ccfa62f5da
ishi <berugen1st@gmail.com>
2023-10-04 07:23:40 +0700

    separate JSONHandler to outside module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 mod constants;
+mod handler;
 mod helpers;
+use handler::JSONHandler;
 use helpers::MemorizeHelper;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::{
-    fs,
-    fs::{File, OpenOptions},
-    path::Path,
-};
+use std::{fs, fs::File, path::Path};
 
 #[derive(Serialize, Deserialize)]
 pub struct MemorizeBox {
@@ -55,35 +53,6 @@ impl MemorizeUtils for MemorizeBox {
             list.iter()
                 .for_each(|item| println!("{}\t{}", item.alias, item.command));
         }
-        Ok(())
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct JSONHandler<'j> {
-    file_path: &'j str,
-}
-
-impl<'j> JSONHandler<'j> {
-    pub fn new(file_path: &'j str) -> Self {
-        JSONHandler { file_path }
-    }
-
-    pub fn read_json_from_file(self) -> Result<Vec<Value>, std::io::Error> {
-        let file = File::open(self.file_path)?;
-        let reader = std::io::BufReader::new(file);
-        let content = serde_json::from_reader(reader)?;
-        Ok(content)
-    }
-
-    pub fn write_into_json(self, content: &Value) -> Result<(), std::io::Error> {
-        let file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(self.file_path)?;
-        let writer = std::io::BufWriter::new(file);
-        serde_json::to_writer_pretty(writer, content)?;
         Ok(())
     }
 }
